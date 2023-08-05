19ff4c5c2e90d92a04bd852dc906d124b7290ef5
Madara09 <dharuntkv@gmail.com>
2023-08-05 13:30:09 +0530

    added a color

--- djangoapi.py
+++ djangoapi.py
@@ -1,5 +1,9 @@
 from tkinter import Tk,Label
 window=Tk()
 window.title("Django")
-window.geometry("200x300")
+window.geometry("450x300")
+
+window.configure(bg="steelblue")
+label=Label(window,text="Welcome!",font={"Arial black",70,"bold"})
+label.pack(pady=100)
 window.mainloop()
