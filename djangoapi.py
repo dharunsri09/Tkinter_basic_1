60ea958cb9219e62d0c72cdc134a7492a4bb269d
Madara09 <dharuntkv@gmail.com>
2023-08-05 22:37:37 +0530

    CLock

--- djangoapi.py
+++ djangoapi.py
@@ -1,9 +1,20 @@
 from tkinter import Tk,Label
+from datetime import datetime
+import tkinter.font as tkFont
 window=Tk()
 window.title("Django")
-window.geometry("450x300")
+window.geometry("600x300")
+
+def clock():
+    time=datetime.now().strftime("%H:%M:%S")
+    label.configure(text=time)
+    label.after(500,clock)
+
 
 window.configure(bg="steelblue")
-label=Label(window,text="Hello",font={"Arial black",70,"bold"})
+font_style = tkFont.Font(family="Arial Black", size=70)
+label=Label(window,font=font_style,bg="steelblue",fg="white")
 label.pack(pady=100)
+
+clock()
 window.mainloop()
